12cf184ff56b488c53f9d55790ebbecaa0228f37
sks <shyamkumarsb007@gmail.com>
2024-07-23 12:00:54 +0530

    basics config setup

--- learn-aurelia-phase1/config.js
+++ learn-aurelia-phase1/config.js
@@ -1,127 +1,172 @@
 System.config({
-  baseURL: "/",
+  baseURL: '/',
   defaultJSExtensions: true,
-  transpiler: "babel",
+  transpiler: 'babel',
   babelOptions: {
-    "optional": [
-      "runtime",
-      "optimisation.modules.system"
-    ]
+    optional: [
+      'runtime',
+      'optimisation.modules.system',
+      'es7.decorators',
+      'es7.classProperties',
+    ],
   },
   paths: {
-    "github:*": "jspm_packages/github/*",
-    "npm:*": "jspm_packages/npm/*"
+    'github:*': 'jspm_packages/github/*',
+    'npm:*': 'jspm_packages/npm/*',
   },
 
   map: {
-    "babel": "npm:babel-core@5.8.38",
-    "babel-runtime": "npm:babel-runtime@5.8.38",
-    "core-js": "npm:core-js@1.2.7",
-    "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.5.1"
+    'aurelia-framework': 'npm:aurelia-framework@1.4.1',
+    babel: 'npm:babel-core@5.8.38',
+    'babel-runtime': 'npm:babel-runtime@5.8.38',
+    'core-js': 'npm:core-js@1.2.7',
+    'github:jspm/nodelibs-assert@0.1.0': {
+      assert: 'npm:assert@1.5.1',
     },
-    "github:jspm/nodelibs-buffer@0.1.1": {
-      "buffer": "npm:buffer@5.7.1"
+    'github:jspm/nodelibs-buffer@0.1.1': {
+      buffer: 'npm:buffer@5.7.1',
     },
-    "github:jspm/nodelibs-path@0.1.0": {
-      "path-browserify": "npm:path-browserify@0.0.0"
+    'github:jspm/nodelibs-path@0.1.0': {
+      'path-browserify': 'npm:path-browserify@0.0.0',
     },
-    "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.10"
+    'github:jspm/nodelibs-process@0.1.2': {
+      process: 'npm:process@0.11.10',
     },
-    "github:jspm/nodelibs-util@0.1.0": {
-      "util": "npm:util@0.10.4"
+    'github:jspm/nodelibs-util@0.1.0': {
+      util: 'npm:util@0.10.4',
     },
-    "github:jspm/nodelibs-vm@0.1.0": {
-      "vm-browserify": "npm:vm-browserify@0.0.4"
+    'github:jspm/nodelibs-vm@0.1.0': {
+      'vm-browserify': 'npm:vm-browserify@0.0.4',
     },
-    "npm:assert@1.5.1": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0",
-      "buffer": "github:jspm/nodelibs-buffer@0.1.1",
-      "object.assign": "npm:object.assign@4.1.5",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "util": "npm:util@0.10.4"
+    'npm:assert@1.5.1': {
+      assert: 'github:jspm/nodelibs-assert@0.1.0',
+      buffer: 'github:jspm/nodelibs-buffer@0.1.1',
+      'object.assign': 'npm:object.assign@4.1.5',
+      process: 'github:jspm/nodelibs-process@0.1.2',
+      util: 'npm:util@0.10.4',
     },
-    "npm:babel-runtime@5.8.38": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
+    'npm:aurelia-binding@2.5.4': {
+      'aurelia-logging': 'npm:aurelia-logging@1.5.2',
+      'aurelia-metadata': 'npm:aurelia-metadata@1.0.8',
+      'aurelia-pal': 'npm:aurelia-pal@1.8.2',
+      'aurelia-task-queue': 'npm:aurelia-task-queue@1.3.3',
     },
-    "npm:buffer@5.7.1": {
-      "base64-js": "npm:base64-js@1.5.1",
-      "ieee754": "npm:ieee754@1.2.1"
+    'npm:aurelia-dependency-injection@1.5.2': {
+      'aurelia-metadata': 'npm:aurelia-metadata@1.0.8',
+      'aurelia-pal': 'npm:aurelia-pal@1.8.2',
     },
-    "npm:call-bind@1.0.7": {
-      "es-define-property": "npm:es-define-property@1.0.0",
-      "es-errors": "npm:es-errors@1.3.0",
-      "function-bind": "npm:function-bind@1.1.2",
-      "get-intrinsic": "npm:get-intrinsic@1.2.4",
-      "set-function-length": "npm:set-function-length@1.2.2"
+    'npm:aurelia-framework@1.4.1': {
+      'aurelia-binding': 'npm:aurelia-binding@2.5.4',
+      'aurelia-dependency-injection': 'npm:aurelia-dependency-injection@1.5.2',
+      'aurelia-loader': 'npm:aurelia-loader@1.0.2',
+      'aurelia-logging': 'npm:aurelia-logging@1.5.2',
+      'aurelia-metadata': 'npm:aurelia-metadata@1.0.8',
+      'aurelia-pal': 'npm:aurelia-pal@1.8.2',
+      'aurelia-path': 'npm:aurelia-path@1.1.7',
+      'aurelia-task-queue': 'npm:aurelia-task-queue@1.3.3',
+      'aurelia-templating': 'npm:aurelia-templating@1.11.1',
     },
-    "npm:core-js@1.2.7": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.2"
+    'npm:aurelia-loader@1.0.2': {
+      'aurelia-metadata': 'npm:aurelia-metadata@1.0.8',
+      'aurelia-path': 'npm:aurelia-path@1.1.7',
     },
-    "npm:define-data-property@1.1.4": {
-      "es-define-property": "npm:es-define-property@1.0.0",
-      "es-errors": "npm:es-errors@1.3.0",
-      "gopd": "npm:gopd@1.0.1"
+    'npm:aurelia-metadata@1.0.8': {
+      'aurelia-pal': 'npm:aurelia-pal@1.8.2',
     },
-    "npm:define-properties@1.2.1": {
-      "define-data-property": "npm:define-data-property@1.1.4",
-      "has-property-descriptors": "npm:has-property-descriptors@1.0.2",
-      "object-keys": "npm:object-keys@1.1.1"
+    'npm:aurelia-task-queue@1.3.3': {
+      'aurelia-pal': 'npm:aurelia-pal@1.8.2',
     },
-    "npm:es-define-property@1.0.0": {
-      "get-intrinsic": "npm:get-intrinsic@1.2.4"
+    'npm:aurelia-templating@1.11.1': {
+      'aurelia-binding': 'npm:aurelia-binding@2.5.4',
+      'aurelia-dependency-injection': 'npm:aurelia-dependency-injection@1.5.2',
+      'aurelia-loader': 'npm:aurelia-loader@1.0.2',
+      'aurelia-logging': 'npm:aurelia-logging@1.5.2',
+      'aurelia-metadata': 'npm:aurelia-metadata@1.0.8',
+      'aurelia-pal': 'npm:aurelia-pal@1.8.2',
+      'aurelia-path': 'npm:aurelia-path@1.1.7',
+      'aurelia-task-queue': 'npm:aurelia-task-queue@1.3.3',
+      process: 'github:jspm/nodelibs-process@0.1.2',
     },
-    "npm:get-intrinsic@1.2.4": {
-      "es-errors": "npm:es-errors@1.3.0",
-      "function-bind": "npm:function-bind@1.1.2",
-      "has-proto": "npm:has-proto@1.0.3",
-      "has-symbols": "npm:has-symbols@1.0.3",
-      "hasown": "npm:hasown@2.0.2"
+    'npm:babel-runtime@5.8.38': {
+      process: 'github:jspm/nodelibs-process@0.1.2',
     },
-    "npm:gopd@1.0.1": {
-      "get-intrinsic": "npm:get-intrinsic@1.2.4"
+    'npm:buffer@5.7.1': {
+      'base64-js': 'npm:base64-js@1.5.1',
+      ieee754: 'npm:ieee754@1.2.1',
     },
-    "npm:has-property-descriptors@1.0.2": {
-      "es-define-property": "npm:es-define-property@1.0.0"
+    'npm:call-bind@1.0.7': {
+      'es-define-property': 'npm:es-define-property@1.0.0',
+      'es-errors': 'npm:es-errors@1.3.0',
+      'function-bind': 'npm:function-bind@1.1.2',
+      'get-intrinsic': 'npm:get-intrinsic@1.2.4',
+      'set-function-length': 'npm:set-function-length@1.2.2',
     },
-    "npm:hasown@2.0.2": {
-      "function-bind": "npm:function-bind@1.1.2"
+    'npm:core-js@1.2.7': {
+      fs: 'github:jspm/nodelibs-fs@0.1.2',
+      path: 'github:jspm/nodelibs-path@0.1.0',
+      process: 'github:jspm/nodelibs-process@0.1.2',
+      'systemjs-json': 'github:systemjs/plugin-json@0.1.2',
     },
-    "npm:inherits@2.0.3": {
-      "util": "github:jspm/nodelibs-util@0.1.0"
+    'npm:define-data-property@1.1.4': {
+      'es-define-property': 'npm:es-define-property@1.0.0',
+      'es-errors': 'npm:es-errors@1.3.0',
+      gopd: 'npm:gopd@1.0.1',
     },
-    "npm:object.assign@4.1.5": {
-      "call-bind": "npm:call-bind@1.0.7",
-      "define-properties": "npm:define-properties@1.2.1",
-      "has-symbols": "npm:has-symbols@1.0.3",
-      "object-keys": "npm:object-keys@1.1.1"
+    'npm:define-properties@1.2.1': {
+      'define-data-property': 'npm:define-data-property@1.1.4',
+      'has-property-descriptors': 'npm:has-property-descriptors@1.0.2',
+      'object-keys': 'npm:object-keys@1.1.1',
     },
-    "npm:path-browserify@0.0.0": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
+    'npm:es-define-property@1.0.0': {
+      'get-intrinsic': 'npm:get-intrinsic@1.2.4',
     },
-    "npm:process@0.11.10": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "vm": "github:jspm/nodelibs-vm@0.1.0"
+    'npm:get-intrinsic@1.2.4': {
+      'es-errors': 'npm:es-errors@1.3.0',
+      'function-bind': 'npm:function-bind@1.1.2',
+      'has-proto': 'npm:has-proto@1.0.3',
+      'has-symbols': 'npm:has-symbols@1.0.3',
+      hasown: 'npm:hasown@2.0.2',
     },
-    "npm:set-function-length@1.2.2": {
-      "define-data-property": "npm:define-data-property@1.1.4",
-      "es-errors": "npm:es-errors@1.3.0",
-      "function-bind": "npm:function-bind@1.1.2",
-      "get-intrinsic": "npm:get-intrinsic@1.2.4",
-      "gopd": "npm:gopd@1.0.1",
-      "has-property-descriptors": "npm:has-property-descriptors@1.0.2"
+    'npm:gopd@1.0.1': {
+      'get-intrinsic': 'npm:get-intrinsic@1.2.4',
     },
-    "npm:util@0.10.4": {
-      "inherits": "npm:inherits@2.0.3",
-      "process": "github:jspm/nodelibs-process@0.1.2"
+    'npm:has-property-descriptors@1.0.2': {
+      'es-define-property': 'npm:es-define-property@1.0.0',
     },
-    "npm:vm-browserify@0.0.4": {
-      "indexof": "npm:indexof@0.0.1"
-    }
-  }
+    'npm:hasown@2.0.2': {
+      'function-bind': 'npm:function-bind@1.1.2',
+    },
+    'npm:inherits@2.0.3': {
+      util: 'github:jspm/nodelibs-util@0.1.0',
+    },
+    'npm:object.assign@4.1.5': {
+      'call-bind': 'npm:call-bind@1.0.7',
+      'define-properties': 'npm:define-properties@1.2.1',
+      'has-symbols': 'npm:has-symbols@1.0.3',
+      'object-keys': 'npm:object-keys@1.1.1',
+    },
+    'npm:path-browserify@0.0.0': {
+      process: 'github:jspm/nodelibs-process@0.1.2',
+    },
+    'npm:process@0.11.10': {
+      assert: 'github:jspm/nodelibs-assert@0.1.0',
+      fs: 'github:jspm/nodelibs-fs@0.1.2',
+      vm: 'github:jspm/nodelibs-vm@0.1.0',
+    },
+    'npm:set-function-length@1.2.2': {
+      'define-data-property': 'npm:define-data-property@1.1.4',
+      'es-errors': 'npm:es-errors@1.3.0',
+      'function-bind': 'npm:function-bind@1.1.2',
+      'get-intrinsic': 'npm:get-intrinsic@1.2.4',
+      gopd: 'npm:gopd@1.0.1',
+      'has-property-descriptors': 'npm:has-property-descriptors@1.0.2',
+    },
+    'npm:util@0.10.4': {
+      inherits: 'npm:inherits@2.0.3',
+      process: 'github:jspm/nodelibs-process@0.1.2',
+    },
+    'npm:vm-browserify@0.0.4': {
+      indexof: 'npm:indexof@0.0.1',
+    },
+  },
 });
--- learn-aurelia-phase1/npm_installs.cmd
+++ learn-aurelia-phase1/npm_installs.cmd
@@ -1,21 +1,46 @@
 @echo off
+:: Suppress the display of commands as they are executed
+
 :: Install global npm packages
+:: This installs the specified version of JSPM globally for use in various projects
 npm install -g jspm@0.16.55 
+
+:: This installs http-server and rimraf globally, which are also useful tools
 npm install -g http-server rimraf
 
 :: Initialize a new npm project
+:: This creates a package.json file with default settings, allowing you to manage project dependencies
 npm init -y
 
+:: Install JSPM locally for this project
+:: Although JSPM is installed globally, it's also installed locally as a project dependency for better version control
 :: Here JSPM is installed as a global command-line tool to mainly use it as a Package manager(here we may use npm also).
 :: In addition to that, we can use it for other functionalities such as 'Module loader', 'Transpiler / polyfills'.
 :: To use these, it's better to install JSPM locally.
 npm install jspm@0.16.55 --save
 
+:: Delete the node_modules folder if it exists
+:: rimraf is used here to clean up the node_modules folder efficiently. This step is optional and depends on whether you need to reset your dependencies
 :: Command to delete the node_modules folder efficiently if not required
 rimraf .\node_modules\ 
 
-:: install packages
+:: Install packages listed in package.json
+:: This installs all dependencies specified in the package.json file
 npm install 
 
-:: Configure the JSPM registry with GitHub
+:: Configure the JSPM registry to use GitHub
+:: This sets up JSPM to fetch packages from GitHub, which might be needed depending on your package sources
 jspm registry config github
+
+:: Initialize JSPM
+:: This sets up JSPM configuration files for the project, which may involve creating jspm.config.js and other related files
+jspm init
+
+:: Install the aurelia-framework package using JSPM
+:: This command installs the aurelia-framework library via JSPM, making it available for use in your project
+jspm install aurelia-framework
+
+:: Install packages using JSPM
+:: This command installs the packages specified in the jspm.config.js file or other JSPM configuration files.
+:: It fetches and installs the necessary dependencies for your project as defined in JSPM's configuration.
+jspm install
