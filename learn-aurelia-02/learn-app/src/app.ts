71b9406fb1e32b238c566fca051d5007aa5fff97
sks <shyamkumarsb007@gmail.com>
2024-08-06 07:38:55 +0530

    routing basics

--- learn-aurelia-02/learn-app/src/app.ts
+++ learn-aurelia-02/learn-app/src/app.ts
@@ -1,7 +1,22 @@
 import { PLATFORM } from 'aurelia-pal';
 import { useView } from 'aurelia-templating';
+import { RouterConfiguration, Router } from 'aurelia-router';
 
 @useView(PLATFORM.moduleName('app.html'))
 export class App {
+
+  constructor(private router: Router) {
+
+  }
+
   public message = '> message from App - [Root]';
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router;
+    config.title = 'Events';
+    config.map([
+      { route: ['', 'home', 'events'], name: 'Home', moduleId: PLATFORM.moduleName('./events/events'), nav: true, title: 'This is the title' }
+    ]);
+  }
+
 }
